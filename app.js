37efd9a75d00bbbf93a696e30591e3c72ca23cb0
cortana <mf2u19@soton.ac.uk>
2020-08-04 14:25:30 +0300

    Level 1 - Username and Password Only

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
-// Require all the packages
-require('dotenv').config();
+//jshint esversion:6
+
+// Require all usual packages
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-//const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
 
-// Create Express app
+// Create app
 const app = express();
 
 // Make the app use some of the packages
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-// Connect to MongoDB database
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-// Schema for an Uer
+// Connect to local mongodb db
+mongoose.connect("mongodb://localhost:27017/userDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Schema for an User
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
-// Encrypt the db
-//userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
-
 // User model
-const User = mongoose.model("List", userSchema);
+const User = new mongoose.model("User", userSchema);
 
-// Render the pages
+// Render the pages that don't need authentication
 app.get("/", function (req, res) {
     res.render("home");
 });
@@ -43,40 +45,50 @@ app.get("/register", function (req, res) {
     res.render("register");
 });
 
+// Register users 
 app.post("/register", function (req, res) {
+
+    // Create new user
     const user = new User({
         email: req.body.username,
         password: req.body.password
     });
 
-    user.save(function(err) {
-        if (!err) {
-            console.log("User saved successfully");
-            res.render("secrets");
-        } else {
+    // Save the user to db
+    user.save(function (err) {
+        if (err) {
             console.log(err);
+        } else {
+            res.render("secrets");
         }
     });
 });
 
+// Login users
 app.post("/login", function (req, res) {
+
+    // Get the username and the password they entered
     const username = req.body.username;
     const password = req.body.password;
 
-    User.findOne({email: username}, function (err, foundUser) {
-        if (!err) {
-            if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
+    // Find a user with that username
+    User.findOne({ email: username }, 
+        function (err, foundUser) {
+            if (err) {
+                console.log(err);
+            } else {
+                if (foundUser) {
+                    // If user was found check if password was correct
+                    if (foundUser.password === password) {
+                        res.render("secrets");
+                    }
                 }
             }
-        } else {
-            console.log(err);
         }
-    });
+    );
 });
 
-// Listen on port 3000
-app.listen(3000, function() {
-    console.log("Server started successfully");
+// Listen on local port 3000
+app.listen(3000, function () {
+    console.log("Server started on port 3000.");
 });
